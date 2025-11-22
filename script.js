559cf70dcbcaea49b1369c43af21781dfa6cdb1c
Bhavesh Suresh <149222281+Bhavveshhh69@users.noreply.github.com>
2025-11-22 11:18:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-const makeChange = (c) => {
-  // your name here
-};
+function makeChange(amount) {
+    let change = {};
+
+    change.q = Math.floor(amount / 25);
+    amount %= 25;
+
+    change.d = Math.floor(amount / 10);
+    amount %= 10;
+
+    change.n = Math.floor(amount / 5);
+    amount %= 5;
+
+    change.p = amount;
+
+    return change;
+}
 
 // Do not the change the code below
 const c = prompt("Enter c: ");
